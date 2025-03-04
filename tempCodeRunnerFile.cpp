5060a90844775979dc7c8239fd7131daa353e351
AkhmadRezkyUtama261 <ahkmadrezkyutama369@gmail.com>
2025-03-04 11:47:43 +0700

    Introduction C++

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+char jeniskelamin = 'L';
+string jenis = "laki laki";
+float x = 10.5;
+int y = 5;
+bool check = true;
+
+cout << "Selamat Datang di TI UMYeah\n";
+cout << "Yogyakarta" << endl;
+cout << "Indonesia";
+}
